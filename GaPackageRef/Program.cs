d3c161b4ea8393dd15c0d1832b74e7932c9a23fd
akihiro taguchi <info@zio3.net>
2021-01-02 10:28:59 +0900

    プロジェクト ファイルを追加します。

---
+++ GaPackageRef/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GaPackageRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
